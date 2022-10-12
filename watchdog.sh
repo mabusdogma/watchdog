fe2b1ac035db836dad78b3ea078171396210fad9
mabusdogma <mabusdogma@gmail.com>
2022-10-12 12:36:10 +0200

    Borrados

--- watchdog.sh
+++
@@ -1,3 +0,0 @@
-date "+%d/%m/%y   %H:%M"
-cd /home/<user>
-sudo ./watchdog.py
--- weekly_restart.sh
+++
@@ -1,6 +0,0 @@
-date "+%d/%m/%y   %H:%M"
-cd /home/<user>
-sudo ./weekly_restart.py
-sleep 1m
-echo "     -- Se reinicia ordenardor"
-reboot
